127c6747f32e9c09ea1c2001ff4acf0b00f4bc74
Evan Pacini <evan.pacini@gmail.com>
2023-01-26 08:40:04 +0100

    Replace handlebars with EJS

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require("express");
-const exphbs = require("express-handlebars");
-//const bodyParser = require('body-parser'); // No longer Required
-//const mysql = require('mysql'); // Not required -> moved to userController
+const expressLayouts = require("express-ejs-layouts");
 
 require("dotenv").config();
 
@@ -10,28 +8,18 @@ const port = process.env.PORT || 5000;
 
 // Parsing middleware
 // Parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.urlencoded({ extended: true })); // New
 
 // Parse application/json
-// app.use(bodyParser.json());
 app.use(express.json()); // New
 
 // Static Files
 app.use(express.static("public"));
 
 // Templating Engine
-const handlebars = exphbs.create({ extname: ".hbs" });
-app.engine(".hbs", handlebars.engine);
-app.set("view engine", ".hbs");
-
-// You don't need the connection here as we have it in userController
-// let connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_NAME
-// });
+app.use(expressLayouts);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
 
 const routes = require("./routes/user");
 app.use("/", routes);
